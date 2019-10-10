90801a3f69736e19b017d424eb22a24d84712ef3
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-10-10 20:51:21 -0300

    Enable cors

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import cors from 'cors';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ ok: true }));
+// Middlewares
+routes.use(cors());
+
+// Users
+routes.post('/users', UserController.store);
+
 
 export default routes;
