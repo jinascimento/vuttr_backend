ba25b658ab9cf9161ae085be8a5c49287bb97527
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-10-10 20:10:48 -0300

    Initial configs with Nodemon and sucrase

---
+++ src/app.js
@@ -0,0 +1,41 @@
+// import 'dotenv/config';
+
+import express from 'express';
+// import * as Sentry from '@sentry/node';
+import path from 'path';
+import routes from './routes';
+// import sentryConfig from './config/Sentry';
+// import './database';
+
+class App {
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+    // this.exceptionHandler();
+  }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+    // this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  // exceptionHandler() {
+  //   this.server.use(async (err, req, res, next) => {
+  //     if (process.env.NODE_ENV === 'development') {
+  //       const errors = await new Youch(err, req).toJSON();
+  //       return res.status(500).json(errors);
+  //     }
+  //
+  //     return res.status(500).json({ error: 'Internal server error'});
+  //
+  //
+  //   });
+  // }
+}
+
+export default new App().server;
